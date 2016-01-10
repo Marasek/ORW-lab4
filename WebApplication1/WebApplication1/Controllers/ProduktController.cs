5fa090d8ec7fec78af2e55f62d931f5a534f3b4f
Marek Tryner <marektryner@wp.pl>
2016-01-10 18:51:15 +0100

    First commit

---
+++ WebApplication1/WebApplication1/Controllers/ProduktController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ProduktController : ApiController
+    {
+        private List<Produkt> produkty = new List<Produkt>() 
+        {
+            new Produkt{Id = 1, Nazwa = "T-Shirt", Cena = 20, Rozmiar = "L" },
+            new Produkt{Id = 2, Nazwa = "T-Shirt 2", Cena = 25, Rozmiar = "XL" },
+            new Produkt{Id = 3, Nazwa = "T-Shirt 3", Cena = 30, Rozmiar = "XXL"}
+        };
+        public IEnumerable<Produkt> Get()
+        {
+            return produkty.ToList();
+        }
+        public IHttpActionResult Get(int id)
+        {
+            var produkt = produkty.Where(x => x.Id == id);
+            if (produkt == null)
+                return NotFound();
+            return Ok(produkt);
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Produkt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Produkt
+    {
+        public int Id { get; set; }
+        public string Nazwa { get; set; }
+        public decimal Cena { get; set; }
+        public string Rozmiar { get; set; }
+    }
+}
